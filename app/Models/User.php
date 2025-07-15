4b1d743d0dd9b0027d6fc014b13bf1bd1f09ea77
Jay Ramilo <webmarket2k@gmail.com>
2025-07-15 20:03:31 +0800

    update user access

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser // <-- Add "implements FilamentUser"
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -53,4 +54,11 @@ class User extends Authenticatable
     return $this->hasMany(User::class, 'company_id', 'company_id')
                 ->where('id', '!=', $this->id);
     }
+
+    public function canAccessPanel(\Filament\Panel $panel): bool
+    {
+    // Allow access only if the user's role is 'Super Admin' or 'Manager'
+    return in_array($this->role, ['Super Admin', 'Manager']);
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,14 @@ Route::get('/', function () {
 });
 
 
-// This route will show the test page form
+// These routes are now PUBLIC and do not require a login.
 Route::get('/commands/test', [CommandController::class, 'showTestForm'])->name('commands.test.show');
-
-// This route will handle the form submission
 Route::post('/commands/test', [CommandController::class, 'executeTestCommand'])->name('commands.test.execute');
+
+
+// This group remains PROTECTED for Admins/Managers ONLY.
+Route::middleware(['auth', 'is.superadmin'])->group(function () {
+    Route::get('/commands/create', [CommandController::class, 'create'])->name('commands.create');
+    Route::post('/commands', [CommandController::class, 'store'])->name('commands.store');
+});
+
